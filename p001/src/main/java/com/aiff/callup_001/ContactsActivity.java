30cde4311aefb6f5d70135ca094ecc8371d5ae83
aiff22 <andrey.ignatoff@gmail.com>
2015-05-22 15:37:53 +0300

    Fixed

--- p001/src/main/java/com/aiff/callup_001/ContactsActivity.java
+++ p001/src/main/java/com/aiff/callup_001/ContactsActivity.java
@@ -34,9 +34,6 @@ import java.util.concurrent.ExecutionException;
 
 public class ContactsActivity extends Activity implements View.OnClickListener {
 
-    private Timer mTimer;
-    private MyTimerTask mMyTimerTask;
-
     // -- The path where user login and password are located -->
 
     final String userLoginKey = "com.aiff.callup_001.app.login";
@@ -187,137 +184,11 @@ public class ContactsActivity extends Activity implements View.OnClickListener {
 
         // -- Add timer to check for new events -->
 
-        mTimer = new Timer();
-        mMyTimerTask = new MyTimerTask();
-        mTimer.schedule(mMyTimerTask, 5000, 5000);
     }
 
     // -- Timer for checking new events -->
 
-    class MyTimerTask extends TimerTask {
 
-        @Override
-        public void run() {
-
-            runOnUiThread(new Runnable() {
-
-                @Override
-                public void run() {
-
-                    SharedPreferences prefs = ContactsActivity.this.getSharedPreferences(
-                            "com.aiff.callup_001.app", Context.MODE_PRIVATE);
-
-                    final String userLoginKey = "com.aiff.callup_001.app.login";
-                    final String userPassKey = "com.aiff.callup_001.app.pass";
-
-                    String storedLogin = prefs.getString(userLoginKey, new String());
-                    String storedPass = prefs.getString(userPassKey, new String());
-
-                    final String[] args = new String[6];
-                    args[0] = "hello";
-                    args[1] = storedLogin;
-                    args[2] = storedPass;
-
-                    try {
-
-                        String res = new ConnectServer().execute(args).get();
-
-                        UserData g = UserData.getInstance();
-                        List<List<String>> data = g.getNewEvents();
-
-                        for (int i = 0; i < data.size(); i++) {
-
-                            final List<String> d = new ArrayList<String>(data.get(i));
-                            if (d.get(0).equals("events")) {
-
-                                switch (d.get(3)) {
-
-                                    case "1":   // new message
-                                        d.set(0, "messages");
-                                        d.set(3, "2");
-                                        List<List<String>> addData1 = new ArrayList<>();
-                                        addData1.add(d);
-                                        g.setData(addData1);
-
-                                        Toast.makeText(ContactsActivity.this, "New incoming message", Toast.LENGTH_SHORT).show();
-                                        Log.d("ContactsLogs:events", String.valueOf(g.getEvents()));
-
-                                        break;
-
-                                    case "2":   // new incoming call
-                                        callResponse(d.get(1), d.get(4));
-                                        break;
-
-                                    case "3":   // missing call
-                                        d.set(0, "calls");
-                                        d.set(2, d.get(4));
-                                        d.set(3, "3");
-                                        d.remove(4);
-                                        List<List<String>> addData3 = new ArrayList<>();
-                                        addData3.add(d);
-                                        g.setData(addData3);
-
-                                        Toast.makeText(ContactsActivity.this, "Missing call", Toast.LENGTH_SHORT).show();
-                                        break;
-
-                                    case "4":   // new contact
-
-                                        if (g.findContact(d.get(1)).equals("-1")) {
-                                            d.set(0, "contacts");
-                                            d.set(3, "3");
-                                            d.remove(4);
-                                            List<List<String>> addData4 = new ArrayList<>();
-                                            addData4.add(d);
-                                            g.setData(addData4);
-                                        } else g.addContact(d.get(1), "contactRequest");
-
-                                        Toast.makeText(ContactsActivity.this, "New contact request", Toast.LENGTH_SHORT).show();
-                                        break;
-
-                                    case "5":
-                                        g.addContact(d.get(1), "confirm");
-                                        Toast.makeText(ContactsActivity.this, "Contact " + String.valueOf(g.findContact(d.get(1))) +
-                                                " accepted your friend request", Toast.LENGTH_SHORT).show();
-                                        break;
-
-                                    case "6":
-                                        g.addContact(d.get(1), "deleteFromFriends");
-                                        break;
-
-                                }
-                            }
-                            if (d.get(0).equals("friends_online")) {
-                                List<List<String>> addContacts = new ArrayList<>();
-                                addContacts.add(d);
-                                List<String> lastOnline = g.getFriendsOnline();
-
-                                if (!lastOnline.equals(d)) {
-                                    g.setData(addContacts);
-                                    Log.d("ContactsActivity", "updateContactStatus");
-                                    TabActivity tab = (TabActivity) getParent();
-                                    String tag = tab.getTabHost().getCurrentTabTag();
-                                    Log.d("ContactsActivity", tag);
-                                    tab.getTabHost().setCurrentTabByTag("tag4");
-                                    tab.getTabHost().setCurrentTabByTag(tag);
-                                }
-
-                                //          TabActivity tab = (TabActivity) getParent();
-                                //          tab.getTabHost().setCurrentTabByTag("tag2");
-                                //          tab.getTabHost().setCurrentTabByTag("tag1");
-                            }
-                        }
-
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    } catch (ExecutionException e) {
-                        e.printStackTrace();
-                    }
-
-                }
-            });
-        }
-
-    }
 
     // -- Change visibility of the options -->
 
@@ -735,88 +606,5 @@ public class ContactsActivity extends Activity implements View.OnClickListener {
         alert.show();
     }
 
-    private void callResponse(final String phone, String time) {
-
-        SharedPreferences prefs = ContactsActivity.this.getSharedPreferences(
-                "com.aiff.callup_001.app", Context.MODE_PRIVATE);
-
-        String storedLogin = prefs.getString(userLoginKey, new String());
-        String storedPass = prefs.getString(userPassKey, new String());
-
-        final String[] args = new String[6];
-        args[0] = "respcall";
-        args[1] = storedLogin;
-        args[2] = storedPass;
-        args[3] = phone;
-        final UserData g = UserData.getInstance();
-
-        AlertDialog.Builder alert = new AlertDialog.Builder(ContactsActivity.this);
-
-        alert.setTitle(phone + " is calling...");
-        if (!g.findContact(phone).equals("-1"))
-            alert.setTitle(g.findContact(phone) + " is calling...");
-
-        final List<List<String>> data = new ArrayList<List<String>>();
-        final List<String> d = new ArrayList<String>();
-
-        d.add("calls");
-        d.add(phone);
-        d.add(time);
-
-        g.delCall(phone, "2");
-        g.delCall(phone, "3");
-
-        alert.setPositiveButton("Accept", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-
-                args[4] = "1";
-                d.add("2");
-                data.add(d);
-                g.setData(data);
-
-                try {
-
-                    String res = new ConnectServer().execute(args).get();
-
-
-                    if (!res.equals("-1") && !res.equals("-3")) {
-                        Intent intentStartCall = new Intent(ContactsActivity.this, CallingActivity.class);
-                        intentStartCall.putExtra("contact", phone);
-                        intentStartCall.putExtra("room", res);
-                        startActivity(intentStartCall);
-                    } else {
-                        Toast.makeText(ContactsActivity.this, "Call cancelled", Toast.LENGTH_SHORT).show();
-                    }
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                } catch (ExecutionException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
-        alert.setNegativeButton("Decline", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-
-                args[4] = "0";
-                d.add("3");
-                data.add(d);
-                g.setData(data);
-
-                try {
-                    String res = new ConnectServer().execute(args).get();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                } catch (ExecutionException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
-        alert.show();
-
-    }
 }
 
